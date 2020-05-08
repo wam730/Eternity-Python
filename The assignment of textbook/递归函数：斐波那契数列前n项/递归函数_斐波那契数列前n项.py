9125b10ba57ecf2d19ddf4094097ce8ec8527580
wam730 <wyj1260419835@gmail.com>
2020-05-08 21:51:02 +0800

    Updated in 2020.5.8

--- The assignment of textbook/递归函数：斐波那契数列前n项/递归函数_斐波那契数列前n项.py
+++ The assignment of textbook/递归函数：斐波那契数列前n项/递归函数_斐波那契数列前n项.py
@@ -13,11 +13,13 @@ while True:
                 return 1
             else:
                 return Fibonacci(x-1)+Fibonacci(x-2)
+
         def Storge(x):
             data = []
             for i in range(1,x+1):
                 data.append(Fibonacci(i))
             return data
+
         print('All the numbers are:\n',Storge(x),"\n")
         y = input("Do you want to continue? If yes, please input 'y' to continue.If not, input anything to exit. ")
         try:
@@ -25,3 +27,19 @@ while True:
         except:
             print("\n")
             break
+
+        #通过另一个函数来调用Fibonacci()函数来实现存储目的不是最好的办法，因为这样会造成很多次重复计算。
+        #比如，计算前10位时，我们需要对每一个数求一次它前面的数才能得到该数，这就是个重复的过程。
+
+        '''def fibo(n):
+	        a=1
+	        b=1
+	        data=[1,1]
+	        for i in range(n-2):
+		        c = a+b
+		        a,b = b,c
+		        data.append(c)
+	        return data
+        print(data)'''
+        
+       
